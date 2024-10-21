ea6891136507df1daed6e37dbe3baa2539f1049a
J5Casey <jackhammerjack@hotmail.com>
2024-10-21 15:07:41 -0500

    changed naming

--- scripts/signal_bus.gd
+++ scripts/signal_bus.gd
@@ -4,5 +4,5 @@ signal player_position_changed(position)
 
 signal resource_collected(resource_type, amount)
 
-signal player_hovering_resource(resource_type)
+signal player_hovering_resource(resource)
 signal player_stopped_hovering_resource
