66c99035cee27fb8fd9ce62edfa8de5123bde6a3
J5Casey <jackhammerjack@hotmail.com>
2024-11-28 13:30:08 -0600

    Added change to food color, RIP Rebecca

--- scripts/food_meter.gd
+++ scripts/food_meter.gd
@@ -5,6 +5,8 @@ extends Control
 
 func _process(_delta):
 	food_label.text = "Food: %.1f" % FoodNetwork.total_food
+	food_label.modulate = Color.REBECCA_PURPLE
+	# Not Becca, Rebecca, because she was a big girl
 	var net_rate = FoodNetwork.get_production_rate() - FoodNetwork.get_consumption_rate()
 	rate_label.text = "Rate: %+.1f/s" % net_rate
 	rate_label.modulate = Color.GREEN if net_rate >= 0 else Color.RED
